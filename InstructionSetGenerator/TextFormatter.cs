151884523cc6baddd1512025a18d2e63c1361ce4
Renato <renato.matosinho.martins@gmail.com>
2020-03-03 18:08:17 -0300

    feat: implemented a code formatting class

---
+++ InstructionSetGenerator/TextFormatter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstructionSetGenerator
+{
+    class TextFormatter
+    {
+        public enum IndentChange
+        {
+            None,
+            Increase,
+            Decrease
+        }
+
+        private StringBuilder stringBuilder = new StringBuilder();
+        private string indentString;
+
+        public uint IndentLevel { get; set; } = 0;
+
+        public TextFormatter(string indentString)
+        {
+            this.indentString = indentString;
+        }
+
+        private void InsertIndentation()
+        {
+            for (int i = 0; i < IndentLevel; i++)
+            {
+                stringBuilder.Append(indentString);
+            }
+        }
+
+        public void IncreaseIndentLevel()
+        {
+            IndentLevel++;
+        }
+
+        public bool DecreaseIndentLevel()
+        {
+            if (IndentLevel > 0)
+            {
+                IndentLevel--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AppendLine(string appendLine, IndentChange change)
+        {
+            switch (change)
+            {
+                case IndentChange.Increase:
+                    IndentLevel++;
+                    break;
+                case IndentChange.Decrease:
+                    if (IndentLevel > 0)
+                        IndentLevel--;
+                    break;
+                default: break;
+            }
+            InsertIndentation();
+            stringBuilder.AppendLine(appendLine);
+        }
+
+        public void AppendLine(string appendLine)
+        {
+            InsertIndentation();
+            stringBuilder.AppendLine(appendLine);
+        }
+
+        public override string ToString()
+        {
+            return stringBuilder.ToString();
+        }
+    }
+}
