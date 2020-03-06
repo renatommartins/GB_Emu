787ecf83ed8ce30612b1f3f44b0aef3661030e0e
Renato <renato.matosinho.martins@gmail.com>
2020-03-06 01:21:06 -0300

    feat: add SUB parser

--- InstructionSetGenerator/Program.cs
+++ InstructionSetGenerator/Program.cs
@@ -448,9 +448,57 @@ namespace InstructionSetGenerator
                     WriteInstruction(textFormatter, instruction, index, disassembly, cycles, operandLength, instructionCodeLines);
                 }
             },
+            //SUB
             {
                 "SUB",
-                null
+                (int index, string instruction , TextFormatter textFormatter) =>
+                {
+                    string[] parameters = instruction.Split(' ')[1].Split(',');
+
+                    string disassembly = Regex.Replace(instruction,"n{1,2}", "0x{0:X}");
+
+                    int cycles = 9999;
+                    int operandLength = 8888;
+                    string[] instructionCodeLines = new string[0];
+
+                    if(Regex.IsMatch(parameters[0], "(^A$)|(^B$)|(^C$)|(^D$)|(^E$)|(^F$)|(^H$)|(^L$)"))
+                    {
+                        if(Regex.IsMatch(parameters[1], "(^A$)|(^B$)|(^C$)|(^D$)|(^E$)|(^F$)|(^H$)|(^L$)"))
+                        {
+                            cycles = 1;
+                            operandLength = 0;
+                            instructionCodeLines = new string[]
+                            {
+                                $"gameboy.CPU.ALU.SubtractByte(gameboy.CPU.registers, CPU.IALU.TargetRegister.{parameters[0]}, gameboy.CPU.registers.{parameters[1]}, false);"
+                            };
+                        }
+                        else if(Regex.IsMatch(parameters[1], "^\\(HL\\)$"))
+                        {
+                            cycles = 2;
+                            operandLength = 1;
+                            instructionCodeLines = new string[]
+                            {
+                                $"gameboy.CPU.ALU.SubtractByte(" +
+                                $"gameboy.CPU.registers, " +
+                                $"CPU.IALU.TargetRegister.{parameters[0]}, " +
+                                $"gameboy.memory.ReadByte(gameboy.CPU.registers.HL), " +
+                                $"false" +
+                                $");"
+                            };
+                        }
+                        else
+                        {
+                            cycles = 2;
+                            operandLength = 2;
+                            instructionCodeLines = new string[]
+                            {
+                                $"gameboy.CPU.ALU.SubtractByte(gameboy.CPU.registers, CPU.IALU.TargetRegister.{parameters[0]}, operands[0], false);"
+                            };
+                        }
+                    }
+
+                    WriteInstruction(textFormatter, instruction, index, disassembly, cycles, operandLength, instructionCodeLines);
+                }
             },
             {
                 "SBC",
