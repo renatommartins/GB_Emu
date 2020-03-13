9ba22039a4611bc2ae15b69e25e010291a56a10b
Renato <renato.matosinho.martins@gmail.com>
2020-03-13 02:08:07 -0300

    feat: Add EI parser

--- InstructionSetGenerator/Program.cs
+++ InstructionSetGenerator/Program.cs
@@ -1006,9 +1006,39 @@ namespace InstructionSetGenerator
                     WriteInstruction(textFormatter, instruction, index, disassembly, cycles, operandLength, instructionCodeLines);
                 }
             },
+            //EI
             {
                 "EI",
-                null
+                (int index, string instruction , TextFormatter textFormatter) =>
+                {
+                    string disassembly = Regex.Replace(instruction,"n{1,2}", "0x{0:X}");
+
+                    int cycles = 1;
+                    int operandLength = 0;
+                    string[] instructionCodeLines = new string[]
+                    {
+                        $"gameboy.CPU.IME = true;"
+                    };
+
+                    WriteInstruction(textFormatter, instruction, index, disassembly, cycles, operandLength, instructionCodeLines);
+                }
+            },
+            //Ext Ops
+            {
+                "Ext",
+                (int index, string instruction , TextFormatter textFormatter) =>
+                {
+                    string disassembly = Regex.Replace(instruction,"n{1,2}", "0x{0:X}");
+
+                    int cycles = 0;
+                    int operandLength = 0;
+                    string[] instructionCodeLines = new string[]
+                    {
+                        
+                    };
+
+                    WriteInstruction(textFormatter, instruction, index, disassembly, cycles, operandLength, instructionCodeLines);
+                }
             },
             #endregion
 
